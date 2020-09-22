1ce1a8da8555f8f242cf0dc60385763d22db5b66
Pavel Klečanský <klecanskypavel@gmail.com>
2020-09-22 17:10:43 +0200

    Adding navbar and README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Restaurant page
+## Project for the odin project https://www.theodinproject.com/courses/javascript/lessons/restaurant-page
+## [Design ispiration](https://codepen.io/AnAbstraction/pen/Yewajz)
--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-alert("Test")
+window.addEventListener("scroll", () => {
+  if (window.scrollY) {
+    document.querySelector("nav").classList.add("black");
+  } else {
+    console.log("test");
+    document.querySelector("nav").classList.remove("black");
+  }
+});
