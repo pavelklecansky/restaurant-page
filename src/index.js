f56ed77167fc8a4917b75240b2260029253dc6ff
Pavel Klečanský <klecanskypavel@gmail.com>
2020-09-22 17:32:41 +0200

    Basic website created with html

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ window.addEventListener("scroll", () => {
   if (window.scrollY) {
     document.querySelector("nav").classList.add("black");
   } else {
-    console.log("test");
     document.querySelector("nav").classList.remove("black");
   }
 });
